b62f006aaf09d32410dd95a0844fbe6835cb9901
smerveroglu <sevdimerve@gmail.com>
2020-09-17 00:54:39 +0300

    added assignment-7

---
+++ Assignment-7/src/components/Server/Server.vue
@@ -0,0 +1,24 @@
+<template>
+    <li
+        class="list-group-item"
+        @click="choiceServer">
+        Server #{{ server.id }}
+    </li>
+</template>
+
+<script>
+    import { eventBus } from '/Users/smeroglu/Desktop/Assignment-8/src/main.js';
+
+    export default {
+        props: ['server'],
+        methods: {
+            choiceServer() {
+                eventBus.$emit('choiceServer', this.server);
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
---
+++ Assignment-7/src/components/Server/ServerDetails.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="col-xs-12 col-sm-6">
+        <p v-if="!server">Servers</p>
+        <p v-else>Server #{{ server.id }} selected, Status: {{ server.status }}</p>
+        <hr>
+        <button @click="reset">Change Status</button>
+    </div>
+
+</template>
+
+<script>
+    import { eventBus } from '/Users/smeroglu/Desktop/Assignment-8/src/main.js';
+
+    export default {
+        data: function() {
+            return {
+                server: null
+            }
+        },
+        methods: {
+          reset() {
+              this.server.status = 'Normal';
+          }
+        },
+        created() {
+            eventBus.$on('choiceServer', (server) => {
+                this.server = server;
+            });
+        }
+    }
+</script>
+
+<style>
+
+</style>
