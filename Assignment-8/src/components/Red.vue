2d35928963ffa1a72adbf8db7f45f16df8af2416
smerveroglu <sevdimerve@gmail.com>
2020-09-18 01:49:40 +0300

    added assignment-8

---
+++ Assignment-8/src/components/Red.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <!-- <slot></slot> -->
+        <h1>Red Component</h1>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+    div {
+        border: 1px solid red;
+        background-color: lightcoral;
+        padding: 30px;
+        margin: 20px auto;
+        text-align: center
+    }
+</style>
